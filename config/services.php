fd5d5f853524421f6c92b39a0b5cf1d70ce584ee
Norin <norinrs11@gmail.com>
2025-07-01 23:20:02 +0700

    Implement on Telegram chatbot

--- config/services.php
+++ config/services.php
@@ -14,6 +14,21 @@ return [
     |
     */
 
+
+    'telegram' => [
+        'bot_token' => env('TELEGRAM_BOT_TOKEN'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
